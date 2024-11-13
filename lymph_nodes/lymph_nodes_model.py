d0c151aa46449aecfbc80cae093af5b5e53eeffb
Jakub Pekár <492788@mail.muni.cz>
2024-11-13 21:00:47 +0100

    fix: experiment

--- lymph_nodes/lymph_nodes_model.py
+++ lymph_nodes/lymph_nodes_model.py
@@ -87,7 +87,7 @@ class LymphNodesModel(LightningModule):
         self.test_metrics.reset()
 
         pd.DataFrame(self.predictions).to_parquet("./predictions.parquet", index=False)
-        self.logger.log_artifact("./predictions.parquet")
+        self.logger.experiment.log_artifact("./predictions.parquet")
 
     def predict_step(
         self, batch: Tensor, batch_idx: int, dataloader_idx: int = 0
