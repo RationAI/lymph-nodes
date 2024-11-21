983fa06a21cc67a0badc82f93c6c50e3d116aaae
Jakub Pekár <492788@mail.muni.cz>
2024-11-21 13:43:24 +0100

    feat: visualization of prediction

--- visualizations/prediction_maks.py
+++ visualizations/prediction_maks.py
@@ -3,8 +3,9 @@ from typing import Any
 
 import pandas as pd
 import pyvips
+import ray
 from openslide import OpenSlide
-from rationai.masks import tile_mask, write_big_tiff
+from rationai.masks import process_items, tile_mask, write_big_tiff
 
 from preprocessing.data import get_relative_dir_path
 from preprocessing.utils import get_mpp, mpp_to_ppmm
@@ -31,13 +32,11 @@ def generate_mask(slide: Any, slide_tiles: pd.DataFrame, dest_dir: Path) -> None
 
 
 def prediction_mask(slides: pd.DataFrame, tiles: pd.DataFrame) -> None:
-    # @ray.remote
+    @ray.remote
     def process_slide(slide: Any) -> None:
         dest_dir = Path(
             "data/prediction_mask", get_relative_dir_path(Path(slide.path))
         )  # keep last level
         generate_mask(slide, tiles[tiles["slide_id"] == slide.id], dest_dir)
 
-    process_slide(slides.iloc[0])
-
-    # process_items(slides.itertuples(), process_item=process_slide)
+    process_items(slides.itertuples(), process_item=process_slide, max_concurrent=2)
