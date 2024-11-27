473cf59a4b5f71a7426891af013e93f28ff30354
Jakub Pekár <492788@mail.muni.cz>
2024-11-27 21:01:06 +0100

    feat: inference main

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from typing import Literal
 import mlflow
 
 from preprocessing.calculate_mean_std import calculate_mean_std
-from preprocessing.create_masks import create_training_masks
-from preprocessing.tiling import training_tiler
+from preprocessing.create_masks import create_inference_masks, create_training_masks
+from preprocessing.tiling import inference_tiler, training_tiler
 from visualizations.download_data import download_inference_data
 from visualizations.heatmap import create_heatmaps
 
@@ -25,6 +25,25 @@ def main(triggers: list[Triggres], params: dict) -> None:
 
     if "calculate_mean_std" in triggers:
         calculate_mean_std()
+
+    if "heatmap" in triggers:
+        slides, predictions = download_inference_data(
+            params["inference_slides_uri"], params["predictions_uri"]
+        )
+        # slides = pd.read_parquet("./data/inference/slides.parquet")
+        # predictions = pd.read_parquet("./data/inference/predictions.parquet")
+        create_heatmaps(slides, predictions)
+
+
+def inf_main(triggers: list[Triggres], params: dict) -> None:
+    mlflow.set_tracking_uri("http://mlflow.rationai-mlflow:5000")
+    # mlflow.set_tracking_uri("https://mlflow.rationai.cloud.trusted.e-infra.cz")
+
+    if "create_masks" in triggers:
+        create_inference_masks()
+
+    if "tiling" in triggers:
+        inference_tiler(run_with_masks_id=params["masks_run_id"])
 
     if "heatmap" in triggers:
         slides, predictions = download_inference_data(
