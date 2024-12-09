003704f602f902bb0a5466683714cc30cffbd12c
Jakub Pekár <492788@mail.muni.cz>
2024-12-09 23:37:03 +0100

    feat: resnet 50

--- lymph_nodes/modeling/backbone/__init__.py
+++ lymph_nodes/modeling/backbone/__init__.py
@@ -1,5 +1,5 @@
-from lymph_nodes.modeling.backbone.resnet18 import resnet18
+from lymph_nodes.modeling.backbone.resnet50 import resnet50
 from lymph_nodes.modeling.backbone.vgg16 import vgg16
 
 
-__all__ = ["resnet18", "vgg16"]
+__all__ = ["resnet50", "vgg16"]
