553e5a91925ee2cd22996752574204404cbce039
Jakub Pekár <492788@mail.muni.cz>
2024-12-10 00:09:01 +0100

    fix: resnet 50

--- lymph_nodes/modeling/backbone/resnet50.py
+++ lymph_nodes/modeling/backbone/resnet50.py
@@ -2,6 +2,6 @@ import torchvision
 from torch import nn
 
 
-def resnet18(weights: str | None = None) -> nn.Module:
+def resnet50(weights: str | None = None) -> nn.Module:
     resnet = torchvision.models.resnet50(weights=weights)
     return nn.Sequential(*(list(resnet.children())[:-2]))
