207f0d452943726ac740f2dfc31a17e078ecc809
Jakub Pekár <492788@mail.muni.cz>
2024-12-15 00:00:48 +0100

    feat: test

--- postpro/prediction_heatmap.py
+++ postpro/prediction_heatmap.py
@@ -1,21 +1,71 @@
 from pathlib import Path
-from typing import Any
+from typing import Any, TypeAlias
 
 import mlflow
+import numpy as np
 import pandas as pd
-
-# import ray
+import pyvips
+import ray
+from numpy.typing import NDArray
 from rationai.masks import process_items, write_big_tiff
-from rationai.masks.slide_assembler import heatmap_assembler_avg
+from rationai.masks.slide_assembler import slide_assembler
 
 
-def prediction_heatmap(
-    slides: pd.DataFrame, predictions: pd.DataFrame, dest: str
-) -> None:
-    # @ray.remote
+Acc: TypeAlias = tuple[NDArray, NDArray]
+
+
+def heatmap_assembler_avg(slide: Any, tiles: pd.DataFrame) -> pyvips.Image:
+    def init(slide: Any) -> Acc:
+        heatmap = np.memmap(
+            str(Path(slide.path).stem) + "_heatmap.nmp",
+            dtype=np.float32,
+            mode="w+",
+            shape=(slide.extent_y, slide.extent_x),
+        )
+
+        counts = np.memmap(
+            str(Path(slide.path).stem) + "_count.nmp",
+            dtype=np.uint8,
+            mode="w+",
+            shape=(slide.extent_y, slide.extent_x),
+        )
+
+        return heatmap, counts
+
+    def aggregate(acc: Acc, slide: Any, tile: Any) -> Acc:
+        heatmap, counts = acc
+
+        heatmap[
+            tile.y : tile.y + slide.tile_extent_y, tile.x : tile.x + slide.tile_extent_x
+        ] += tile.probability
+        counts[
+            tile.y : tile.y + slide.tile_extent_y, tile.x : tile.x + slide.tile_extent_x
+        ] += 1
+
+        heatmap.flush()
+        counts.flush()
+
+        return heatmap, counts
+
+    def finalize(acc: Acc) -> pyvips.Image:
+        heatmap, counts = acc
+
+        return pyvips.Image.new_from_memory(
+            np.where(counts, heatmap / counts, 0) * 255,
+            slide.extent_x,
+            slide.extent_y,
+            1,
+            format=pyvips.BandFormat.UCHAR,
+        )
+
+    return slide_assembler(slide, tiles, init, aggregate, finalize)
+
+
+def prediction_heatmap(slides: pd.DataFrame, tiles: pd.DataFrame, dest: str) -> None:
+    @ray.remote
     def process_slide(slide: Any) -> None:
-        slide_predictions = predictions[predictions["slide_id"] == slide.id]
-        mask = heatmap_assembler_avg(slide, slide_predictions, 4)
+        slide_tiles = tiles[tiles["slide_id"] == slide.id]
+        mask = heatmap_assembler_avg(slide, slide_tiles)
 
         mask_path = Path(dest, f"{Path(slide.path).stem}.tiff")
         mask_path.parent.mkdir(exist_ok=True, parents=True)
@@ -27,9 +77,6 @@ def prediction_heatmap(
             mpp_y=slide.mpp_y,
         )
 
-    # process_items(slides.itertuples(), process_item=process_slide, max_concurrent=2)
-    for i, slide in enumerate(slides.itertuples()):
-        print(f"Processing slide {i+1}/{len(slides)}")
-        process_slide(slide)
+    process_items(list(slides.itertuples()), process_item=process_slide)
 
     mlflow.log_artifacts(dest, "heatmaps")
