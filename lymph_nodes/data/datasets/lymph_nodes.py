c5ab2120c2aa2d061dd40974117099b92c3bb1c0
Jakub Pekár <492788@mail.muni.cz>
2025-01-03 08:56:39 +0100

    feat: fix model bug

--- lymph_nodes/data/datasets/lymph_nodes.py
+++ lymph_nodes/data/datasets/lymph_nodes.py
@@ -56,7 +56,7 @@ class LymphNodesPredict(MetaTiledSlides[PredictSample]):
     def generate_datasets(self) -> Iterable[Dataset[PredictSample]]:
         slides_2023 = self.slides[self.slides["path"].str.contains("2023")]["id"]
 
-        self.tiles = self.tiles[self.tiles["slide_id"].isin(slides_2023),].reset_index()
+        self.tiles = self.tiles[self.tiles["slide_id"].isin(slides_2023)].reset_index()
 
         return (
             _LymphNodesSlideTiles(
