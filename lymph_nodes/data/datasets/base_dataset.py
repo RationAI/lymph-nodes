9003ba5e5d721e172f4246989013b68fbdedea7f
Jakub Pekár <492788@mail.muni.cz>
2025-03-26 10:32:37 +0100

    fix: categirical col

--- lymph_nodes/data/datasets/base_dataset.py
+++ lymph_nodes/data/datasets/base_dataset.py
@@ -67,8 +67,10 @@ class BaseDataset(MetaTiledSlides[T], ABC):
             ~self.tiles["cancer"] & (self.tiles["brownish"] > 0.05), "gb-kind"
         ] = "brownish"
         # Make sure it is ordered by cateogry when using order by
-        self.tiles["gb-kind"] = self.tiles["gb-kind"].cat.set_categories(
-            ["cancer", "brownish", "normal"], ordered=True
+        self.tiles["gb-kind"] = (
+            self.tiles["gb-kind"]
+            .astype("category")
+            .cat.set_categories(["cancer", "brownish", "normal"], ordered=True)
         )
 
         self._prepare_data_hook()
