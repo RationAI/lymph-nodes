7eb84c9c6ed3ed1e8aff5ef1b2518522a8f557fa
Jakub Pekár <492788@mail.muni.cz>
2025-05-16 07:31:16 +0000

    feat: tiling

--- prepro/__main__.py
+++ prepro/__main__.py
@@ -7,13 +7,16 @@ import hydra
 import mlflow
 from omegaconf import DictConfig, OmegaConf
 
-from prepro.annotation_masks import generate_annotation_masks
-from prepro.color_separation import generate_color_separation_masks
-from prepro.cyto_ignore_masks import generate_cyto_ignore_masks
+# from prepro.annotation_masks import generate_annotation_masks
+# from prepro.color_separation import generate_color_separation_masks
+# from prepro.cyto_ignore_masks import generate_cyto_ignore_masks
 from prepro.data_source import ChainedDataSources, DataSource
-from prepro.ignore_masks import generate_ignore_masks
+
+# from prepro.ignore_masks import generate_ignore_masks
 from prepro.tiling import Args, tile_dataset
-from prepro.tissue_masks import generate_tissue_masks
+
+
+# from prepro.tissue_masks import generate_tissue_masks
 
 
 @dataclass
@@ -40,25 +43,30 @@ def main(config: DictConfig) -> None:
         artifact_uri=config.metadata.cytokeratin_masks_path, dst_path="./data"
     )
 
-    # mlflow.artifacts.download_artifacts(
-    #     artifact_uri="mlflow-artifacts:/68/b75ae72569654c45891e25a2d58186ce/artifacts/tissue_masks",
-    #     dst_path="./data",
-    # )
+    mlflow.artifacts.download_artifacts(
+        artifact_uri="mlflow-artifacts:/68/10bfc155a303465882aada4928487822/artifacts/tissue_masks",
+        dst_path="./data",
+    )
 
-    # mlflow.artifacts.download_artifacts(
-    #     artifact_uri="mlflow-artifacts:/68/b75ae72569654c45891e25a2d58186ce/artifacts/ignore_masks",
-    #     dst_path="./data",
-    # )
+    mlflow.artifacts.download_artifacts(
+        artifact_uri="mlflow-artifacts:/68/10bfc155a303465882aada4928487822/artifacts/ignore_masks",
+        dst_path="./data",
+    )
 
-    # mlflow.artifacts.download_artifacts(
-    #     artifact_uri="mlflow-artifacts:/68/b75ae72569654c45891e25a2d58186ce/artifacts/annotation_masks",
-    #     dst_path="./data",
-    # )
+    mlflow.artifacts.download_artifacts(
+        artifact_uri="mlflow-artifacts:/68/10bfc155a303465882aada4928487822/artifacts/annotation_masks",
+        dst_path="./data",
+    )
 
-    # mlflow.artifacts.download_artifacts(
-    #     artifact_uri="mlflow-artifacts:/68/04f3267cd2fc4c6bbecbacb3336a13a1/artifacts/color_separation_masks",
-    #     dst_path="./data",
-    # )
+    mlflow.artifacts.download_artifacts(
+        artifact_uri="mlflow-artifacts:/68/10bfc155a303465882aada4928487822/artifacts/cyto_ignore_masks",
+        dst_path="./data",
+    )
+
+    mlflow.artifacts.download_artifacts(
+        artifact_uri="mlflow-artifacts:/68/10bfc155a303465882aada4928487822/artifacts/color_separation_masks",
+        dst_path="./data",
+    )
 
     print("Prepare datasources")
 
@@ -110,8 +118,11 @@ def main(config: DictConfig) -> None:
                 datasource=DataSource(
                     "/mnt/data/Projects/lymph_nodes/dataset1-ihc-2023",
                     glob_pattern=[
-                        "*_20_SLIDE_[0-9]*-0.mrxs",
-                        "*_59_SLIDE_[0-9]*-0.mrxs",
+                        "*_1_SLIDE_[0-9]*-0.mrxs",
+                        "*_5_SLIDE_[0-9]*-0.mrxs",
+                        "*_7_SLIDE_[0-9]*-0.mrxs",
+                        "*_8_SLIDE_[0-9]*-0.mrxs",
+                        "*_105_SLIDE_[0-9]*-0.mrxs",
                     ],
                 ),
                 source_kind="lymph_node",
@@ -122,15 +133,15 @@ def main(config: DictConfig) -> None:
                     "/mnt/data/Projects/lymph_nodes/dataset1-ihc-2023",
                     glob_pattern="*-0.mrxs",
                     exclue_pattern=[
-                        "*_2_SLIDE_[0-9]*-0.mrxs",
-                        "*_3_SLIDE_[0-9]*-0.mrxs",
-                        "*_34_SLIDE_[0-9]*-0.mrxs",
-                        "*_52_SLIDE_[0-9]*-0.mrxs",
-                        "*_80_SLIDE_[0-9]*-0.mrxs",
-                        "*_114_SLIDE_[0-9]*-0.mrxs",
-                        "*_20_SLIDE_[0-9]*-0.mrxs",
-                        "*_59_SLIDE_[0-9]*-0.mrxs",
-                        # Faulty file
+                        "*_1_SLIDE_[0-9]*-0.mrxs",  # test
+                        "*_2_SLIDE_[0-9]*-0.mrxs",  # val
+                        "*_3_SLIDE_[0-9]*-0.mrxs",  # val
+                        "*_5_SLIDE_[0-9]*-0.mrxs",  # test
+                        "*_7_SLIDE_[0-9]*-0.mrxs",  # test
+                        "*_8_SLIDE_[0-9]*-0.mrxs",  # test
+                        "*_52_SLIDE_[0-9]*-0.mrxs",  # val
+                        "*_105_SLIDE_[0-9]*-0.mrxs",  # test
+                        # Faulty files
                         "SNB_IHC_CASE_81_SLIDE_1-0.mrxs",
                         "SNB_IHC_CASE_82_SLIDE_1-0.mrxs",
                     ],
@@ -144,10 +155,7 @@ def main(config: DictConfig) -> None:
                     glob_pattern=[
                         "*_2_SLIDE_[0-9]*-0.mrxs",
                         "*_3_SLIDE_[0-9]*-0.mrxs",
-                        "*_34_SLIDE_[0-9]*-0.mrxs",
                         "*_52_SLIDE_[0-9]*-0.mrxs",
-                        "*_80_SLIDE_[0-9]*-0.mrxs",
-                        "*_114_SLIDE_[0-9]*-0.mrxs",
                     ],
                 ),
                 source_kind="lymph_node",
@@ -162,24 +170,24 @@ def main(config: DictConfig) -> None:
             source_kind="lymph_node",
             slide_metastazis=True,
         ),
-        "lymphs-2024": {
-            "positive": Dataset(
-                datasource=DataSource(
-                    "/mnt/data/Projects/lymph_nodes/dataset2-ihc-2024/positive",
-                    glob_pattern="*-1.mrxs",
-                ),
-                source_kind="lymph_node",
-                slide_metastazis=True,
-            ),
-            "negative": Dataset(
-                datasource=DataSource(
-                    "/mnt/data/Projects/lymph_nodes/dataset2-ihc-2024/negative",
-                    glob_pattern="*-0.mrxs",
-                ),
-                source_kind="lymph_node",
-                slide_metastazis=False,
-            ),
-        },
+        # "lymphs-2024": {
+        #     "positive": Dataset(
+        #         datasource=DataSource(
+        #             "/mnt/data/Projects/lymph_nodes/dataset2-ihc-2024/positive",
+        #             glob_pattern="*-1.mrxs",
+        #         ),
+        #         source_kind="lymph_node",
+        #         slide_metastazis=True,
+        #     ),
+        #     "negative": Dataset(
+        #         datasource=DataSource(
+        #             "/mnt/data/Projects/lymph_nodes/dataset2-ihc-2024/negative",
+        #             glob_pattern="*-0.mrxs",
+        #         ),
+        #         source_kind="lymph_node",
+        #         slide_metastazis=False,
+        #     ),
+        # },
         "tmas": {
             "test": Dataset(
                 datasource=DataSource(
@@ -238,67 +246,67 @@ def main(config: DictConfig) -> None:
     # Masks
 
     # Tissue masks
-    print("Generating tissue masks")
-    generate_tissue_masks(
-        slide_paths=ChainedDataSources(list(map_datasets(datasets))),
-        mpp=2,
-        reference_path=config.metadata.relative_path_prefix,
-        dest=config.metadata.tissue_mask_dest,
-    )
+    # print("Generating tissue masks")
+    # generate_tissue_masks(
+    #     slide_paths=ChainedDataSources(list(map_datasets(datasets))),
+    #     mpp=2,
+    #     reference_path=config.metadata.relative_path_prefix,
+    #     dest=config.metadata.tissue_mask_dest,
+    # )
 
     # Annotation masks
-    print("Generating annotation masks")
-    generate_annotation_masks(
-        slide_paths=ChainedDataSources(
-            [
-                datasets["lymhps-2023"]["positive-train"].datasource,
-                datasets["lymhps-2023"]["positive-val"].datasource,
-                datasets["positive-lymph-nodes"].datasource,
-            ]
-        ),
-        mpp=2,
-        reference_path=config.metadata.relative_path_prefix,
-        dest=config.metadata.annotation_mask_dest,
-    )
+    # print("Generating annotation masks")
+    # generate_annotation_masks(
+    #     slide_paths=ChainedDataSources(
+    #         [
+    #             datasets["lymhps-2023"]["positive-train"].datasource,
+    #             datasets["lymhps-2023"]["positive-val"].datasource,
+    #             datasets["positive-lymph-nodes"].datasource,
+    #         ]
+    #     ),
+    #     mpp=2,
+    #     reference_path=config.metadata.relative_path_prefix,
+    #     dest=config.metadata.annotation_mask_dest,
+    # )
 
     # Ignore masks
-    print("Generating ignore masks")
-    generate_ignore_masks(
-        slide_paths=ChainedDataSources(
-            [
-                datasets["tmas"]["test"].datasource,
-                datasets["tmas"]["train"].datasource,
-                datasets["tmas"]["val"].datasource,
-            ]
-        ),
-        mpp=2,
-        reference_path=config.metadata.relative_path_prefix,
-        dest=config.metadata.ignore_mask_dest,
-    )
+    # print("Generating ignore masks")
+    # generate_ignore_masks(
+    #     slide_paths=ChainedDataSources(
+    #         [
+    #             datasets["tmas"]["test"].datasource,
+    #             datasets["tmas"]["train"].datasource,
+    #             datasets["tmas"]["val"].datasource,
+    #         ]
+    #     ),
+    #     mpp=2,
+    #     reference_path=config.metadata.relative_path_prefix,
+    #     dest=config.metadata.ignore_mask_dest,
+    # )
 
     # CytoIgnore masks
-    print("Generating cyto ignore masks")
-    generate_cyto_ignore_masks(
-        slide_paths=ChainedDataSources(
-            [
-                datasets["tmas"]["test"].datasource,
-                datasets["tmas"]["train"].datasource,
-                datasets["tmas"]["val"].datasource,
-            ]
-        ),
-        mpp=2,
-        reference_path=config.metadata.relative_path_prefix,
-        dest=config.metadata.ignore_mask_dest,
-    )
+    # print("Generating cyto ignore masks")
+    # generate_cyto_ignore_masks(
+    #     slide_paths=ChainedDataSources(
+    #         [
+    #             datasets["tmas"]["test"].datasource,
+    #             datasets["tmas"]["train"].datasource,
+    #             datasets["tmas"]["val"].datasource,
+    #         ]
+    #     ),
+    #     mpp=2,
+    #     reference_path=config.metadata.relative_path_prefix,
+    #     dest=config.metadata.ignore_mask_dest,
+    # )
 
     # Color separation masks
-    print("Generating color separation masks")
-    generate_color_separation_masks(
-        slide_paths=ChainedDataSources(list(map_datasets(datasets))),
-        mpp=2,
-        reference_path=config.metadata.relative_path_prefix,
-        dest=config.metadata.color_separation_mask_dest,
-    )
+    # print("Generating color separation masks")
+    # generate_color_separation_masks(
+    #     slide_paths=ChainedDataSources(list(map_datasets(datasets))),
+    #     mpp=2,
+    #     reference_path=config.metadata.relative_path_prefix,
+    #     dest=config.metadata.color_separation_mask_dest,
+    # )
 
     # Tiling
     print("Tiling")
@@ -323,9 +331,6 @@ def main(config: DictConfig) -> None:
             "annotation_masks_dir": config.metadata.annotation_mask_dest,
             "relative_path_prefix": config.metadata.relative_path_prefix,
         }
-
-    ## Infer
-    print("Tiling inference dataset")
 
     def process_dataset(dataset: dict[str, Any], prefix: str = "") -> None:
         for key in dataset:
