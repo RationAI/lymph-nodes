5e9e133d04180f9a30721493ee8ec11515ce8453
Jakub Pekár <492788@mail.muni.cz>
2025-05-19 00:43:12 +0000

    fix: typo

--- postpro/roc_calculation.py
+++ postpro/roc_calculation.py
@@ -87,7 +87,7 @@ def process_prediction(
         tpr = np.zeros(256)
         fpr = fps / n
 
-    print(tpr, fpr, flust=True)
+    print(tpr, fpr, flush=True)
     np.savetxt(
         Path(f"./data/{run_id}/roc/{prefix}", rel_path, f"{Path(pred_path).stem}.txt"),
         np.array([tpr, fpr]),
