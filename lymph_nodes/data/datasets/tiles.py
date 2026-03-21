e77a941863b2e22bd4b0419aef554b428a72a9d3
Bc Tomáš Homola <567740@mail.muni.cz>
2026-03-21 14:21:22 +0100

    refactor: tiles

--- lymph_nodes/data/datasets/tiles.py
+++ lymph_nodes/data/datasets/tiles.py
@@ -2,15 +2,50 @@ from collections.abc import Iterable
 from pathlib import Path
 from typing import Any
 
+import numpy as np
 from albumentations.core.composition import TransformType
 from albumentations.pytorch import ToTensorV2
 from datasets import Dataset as HFDataset
 from datasets import load_dataset
 from mlflow.artifacts import download_artifacts
-from rationai.mlkit.data.datasets import OpenSlideTilesDataset
+from numpy.typing import NDArray
+from ratiopath.openslide import OpenSlide
 from torch.utils.data import ConcatDataset, Dataset
 
 from lymph_nodes.typing import TileMetadata, TilesPredictSample, TilesSample
+
+
+class OpenSlideTilesDataset(Dataset[NDArray[np.uint8]]):
+    def __init__(
+        self,
+        slide_path: str | Path,
+        level: int | str,
+        tile_extent_x: int | str,
+        tile_extent_y: int | str,
+        tiles: HFDataset,
+    ) -> None:
+        super().__init__()
+        self.slide_path = Path(slide_path)
+        self.level = level
+        self.tile_extent_x = tile_extent_x
+        self.tile_extent_y = tile_extent_y
+        self.tiles = tiles
+
+    def __len__(self) -> int:
+        return len(self.tiles)
+
+    def __getitem__(self, idx: int) -> NDArray[np.uint8]:
+        tile = self.tiles[idx]
+        level = self._get_from_tile(tile, self.level)
+        extent_x = self._get_from_tile(tile, self.tile_extent_x)
+        extent_y = self._get_from_tile(tile, self.tile_extent_y)
+
+        with OpenSlide(self.slide_path) as slide:
+            return slide.read_tile(tile["x"], tile["y"], extent_x, extent_y, level)
+
+    @staticmethod
+    def _get_from_tile(tile: dict[str, Any], key: int | str) -> int:
+        return tile[key] if isinstance(key, str) else key
 
 
 class _Tiles[T: TilesPredictSample | TilesSample](Dataset[T]):
@@ -23,9 +58,9 @@ class _Tiles[T: TilesPredictSample | TilesSample](Dataset[T]):
         super().__init__()
         self.slide_tiles = OpenSlideTilesDataset(
             slide_path=slide_metadata["path"],
-            level=slide_metadata["level"],
-            tile_extent_x="tile_extent_x",
-            tile_extent_y="tile_extent_y",
+            level=slide_metadata.get("level", "level"),
+            tile_extent_x=slide_metadata.get("tile_extent_x", "tile_extent_x"),
+            tile_extent_y=slide_metadata.get("tile_extent_y", "tile_extent_y"),
             tiles=tiles,
         )
         self.slide_metadata = slide_metadata
