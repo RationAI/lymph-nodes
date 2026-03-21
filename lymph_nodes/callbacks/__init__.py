8cbef79baa236ae0d4d8b5406ecdf8034986f737
Bc Tomáš Homola <567740@mail.muni.cz>
2026-03-21 10:53:06 +0100

    feat: visualization callback

---
+++ lymph_nodes/callbacks/__init__.py
@@ -0,0 +1,3 @@
+from lymph_nodes.callbacks.mask_builder import MaskBuilderCallback
+
+__all__ = ["MaskBuilderCallback"]
--- lymph_nodes/data/data_module.py
+++ lymph_nodes/data/data_module.py
@@ -26,6 +26,8 @@ class DataModule(LightningDataModule):
                 self.val = instantiate(self.datasets["val"])
             case "test":
                 self.test = instantiate(self.datasets["test"])
+            case "predict":
+                self.predict = instantiate(self.datasets["predict"])
 
     def train_dataloader(self) -> Iterable[Input]:
         return DataLoader(
@@ -48,4 +50,12 @@ class DataModule(LightningDataModule):
     def test_dataloader(self) -> Iterable[Input]:
         return DataLoader(
             self.test, batch_size=self.batch_size, num_workers=self.num_workers
+        )
+
+    def predict_dataloader(self) -> Iterable[Input]:
+        return DataLoader(
+            self.predict,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            persistent_workers=self.num_workers > 0,
         )
