e211ee966b601f697276fb279329159aeb7f7ccb
Bc Tomáš Homola <567740@mail.muni.cz>
2026-04-14 11:43:37 +0200

    fix: remove embeddings

--- lymph_nodes/data/datasets/__init__.py
+++ lymph_nodes/data/datasets/__init__.py
@@ -7,16 +7,18 @@ from lymph_nodes.data.datasets.subset import (
     TileEmbeddingsSubset,
     create_subset,
 )
-from lymph_nodes.data.datasets.tile_embeddings import TileEmbeddings
-from lymph_nodes.data.datasets.tile_patch import TilePatchDataset
+
+
+# from lymph_nodes.data.datasets.tile_embeddings import TileEmbeddings
+# from lymph_nodes.data.datasets.tile_patch import TilePatchDataset
 
 
 __all__ = [
     "DatasetSubset",
     "MLPEmbeddingDataset",
     "SlideEmbeddingDataset",
-    "TileEmbeddings",
+    # "TileEmbeddings",
     "TileEmbeddingsSubset",
-    "TilePatchDataset",
+    # "TilePatchDataset",
     "create_subset",
 ]
